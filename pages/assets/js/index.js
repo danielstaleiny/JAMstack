afd8a820ca0e28c571f9391a0d1f119d7bf02bf8
Daniel Rafaj <daniel.rafaj@tuta.io>
2020-05-02 17:38:21 +0200

    async examples

--- pages/assets/js/index.js
+++ pages/assets/js/index.js
@@ -1,12 +1,12 @@
 // import('./dynamic').then((js) => js.run())
 
-import pure from './pure/Main'
-// import tool from './pure/Tool'
+// import pure from './pure/Main'
+import tool from './pure/Tool'
 // import dataAddressbook from './pure/Data.AddressBook'
 
 // console.log('Hello world from /js/index.js!')
 
-pure.main()
-// tool.run()
+// pure.main()
+tool.main()
 // dataAddressbook.run()
 //
