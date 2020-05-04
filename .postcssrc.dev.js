0a1189abcd06959f0c21e0819dc3a54acb788abe
Daniel Rafaj <daniel.rafaj@tuta.io>
2020-05-04 15:50:10 +0200

    building works but without parcel,

--- .postcssrc.dev.js
+++ .postcssrc.dev.js
@@ -1,5 +1,5 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['.tmp/parcel/**/*.html'],
+  content: ['_site/**/*.html'],
   // Include any special characters you're using in this regular expression
   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:\.\/]+/g) || [],
 })
